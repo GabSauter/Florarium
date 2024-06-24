90db79d6cab973d76fea4dd123f4f851d41b38de
Sauter <gabriel.andrade.sauter@gmail.com>
2024-06-24 14:25:52 -0300

    refactor pause

--- game/managers/pause/handle_pause.gd
+++ game/managers/pause/handle_pause.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
-@export var pause_menu: PackedScene = null
-@onready var ui_container = $"../../UIContainer"
+@export var pause_menu: PackedScene = preload("res://ui/menu/pause_menu/pause_menu.tscn")
+@onready var ui_container = get_tree().current_scene.get_node("UIContainer")
 
 func _unhandled_key_input(event):
 	if event.is_action_pressed("Pause") and !ui_container.get_children():
