c00c8fac086e0d365b935ca753c596b6df416a7a
Sauter <gabriel.andrade.sauter@gmail.com>
2024-08-25 16:55:32 -0300

    add respawn animated sprite

--- entities/game_related/Respawn.gd
+++ entities/game_related/Respawn.gd
@@ -1,5 +1,10 @@
 extends Area2D
 
+@onready var animated_sprite = $AnimatedSprite2D
+@onready var particles = $GPUParticles2D
+
+func _ready():
+	particles.emitting = false
 # com o is active, faz com que o respawn seja no último checkpoint pego
 # desabilitando os checkpoints anteriores, sendo viável para jogos tipo celeste, mas inviável para jogos tipo hollow knight
 var isActive = false
@@ -8,3 +13,5 @@ func _on_body_entered(body):
 	if body is Player and !isActive:
 		body.respawn_position = global_position
 		isActive = true
+		animated_sprite.play("open_flower")
+		particles.emitting = true
