2713b8c5282856f5aaa6f9bf2b21ec0ba832cb15
Gabriel Sauter <gabriel.andrade.sauter@gmail.com>
2024-10-06 11:07:22 -0300

    add npc

---
+++ entities/NPC/npc.gd
@@ -0,0 +1,30 @@
+extends Area2D
+
+@onready var label: RichTextLabel = $RichTextLabel
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+@export var text: String
+@export var sprites: SpriteFrames
+
+func _ready() -> void:
+	label.visible = false
+	label.text = text
+	animated_sprite.sprite_frames = sprites
+	animated_sprite.play()
+	
+	var current_texture = sprites.get_frame_texture("default", 0)
+	if current_texture:
+		var sprite_height = current_texture.get_size().y
+		var label_size = label.get_minimum_size()
+		label.position = Vector2(label.position.x, animated_sprite.position.y - sprite_height / 2 - label_size.y)
+
+func _process(delta: float) -> void:
+	pass
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is Player:
+		label.visible = true
+
+func _on_body_exited(body: Node2D) -> void:
+	if body is Player:
+		label.visible = false
